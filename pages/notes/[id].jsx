e9f934434977daf793b8ba698b224918e17d9478
Y3rbit4 <hrodriguez@frba.utn.edu.ar>
2021-05-21 03:14:27 -0300

    modificacion env

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -16,7 +16,7 @@ const Note = ({ note }) => {
 export default Note;
 
 export async function getServerSideProps({ params, req, res }) {
-  const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
+  const response = await fetch(`${process.env.API_URL}/api/note/${params.id}`);
   if (!response.ok) {
     res.writeHead(302, { Location: "/notes" });
     res.end();
